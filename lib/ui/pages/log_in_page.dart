5d3733afb31c1cba761a3fae02989c7ce3bf186e
ferneyh57 <ferney1438123@gmail.com>
2022-12-30 12:01:25 -0500

    login base layout

--- lib/ui/pages/log_in_page.dart
+++ lib/ui/pages/log_in_page.dart
@@ -5,45 +5,113 @@ class LogInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('AppBar Demo'),
-          actions: <Widget>[
-            IconButton(
-              icon: const Icon(Icons.add_alert),
-              tooltip: 'Show Snackbar',
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('This is a snackbar')));
-              },
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 120,
+        centerTitle: true,
+        title: Column(
+          children: [
+            Text(
+              'amb app'.toUpperCase(),
             ),
-            IconButton(
-              icon: const Icon(Icons.navigate_next),
-              tooltip: 'Go to the next page',
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute<void>(
-                  builder: (BuildContext context) {
-                    return Scaffold(
-                      appBar: AppBar(
-                        title: const Text('Next page'),
-                      ),
-                      body: const Center(
-                        child: Text(
-                          'This is the next page',
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ),
-                    );
-                  },
-                ));
-              },
-            ),
+            const SizedBox(height: 12),
+            const Text('Login'),
           ],
         ),
-        body: Column(
-          children: [],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ///Field para el telefono.
+            Text(
+              'celular',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 6),
+            TextField(),
+            const SizedBox(height: 24),
+
+            ///Field para la contraseña.
+            Text(
+              'Clave',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            const SizedBox(height: 6),
+            TextField(),
+            const SizedBox(height: 24),
+
+            /// Recordar contraseña.
+            _SwitchWidget(),
+            const Spacer(),
+            const _Buttons(),
+            const Spacer(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _SwitchWidget extends StatefulWidget {
+  const _SwitchWidget();
+
+  @override
+  State<_SwitchWidget> createState() => _SwitchWidgetState();
+}
+
+class _SwitchWidgetState extends State<_SwitchWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Switch(
+      value: false,
+      onChanged: (value) {},
+    );
+  }
+}
+
+class _Buttons extends StatelessWidget {
+  const _Buttons();
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          /// Boton de login.
+          Ink(
+            decoration: BoxDecoration(
+              color: Colors.blue[900],
+              borderRadius: const BorderRadius.all(
+                Radius.circular(8),
+              ),
+            ),
+            child: InkWell(
+              onTap: () {},
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 12, horizontal: 36),
+                child: Text(
+                  'ENTRAR',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Olvido su contraseña?',
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                    color: Colors.blue[900],
+                  ),
+            ),
+          )
+        ],
       ),
     );
   }
